55b29613c27a950b8c80e1b25b59cdd896a2cfc8
siham <sihamkassim735@gmail.com>
2025-09-05 10:55:19 +0300

    minor fixes on merge

--- src/pages/BookDetail.jsx
+++ src/pages/BookDetail.jsx
@@ -1,4 +1,4 @@
-import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { Label } from "@/components/ui/label";
